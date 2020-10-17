879725cdf840f9342f0fd09d1e2cf7c8309401f3
Matthew Gibson <mattgibson6@gmail.com>
2020-10-17 14:45:53 -0600

    add all tables to create.sql

--- LC-wetbat-API/db/create.sql
+++ LC-wetbat-API/db/create.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS customers
 CASCADE;
+DROP TABLE IF EXISTS quotes
+CASCADE;
 
 -- ************************************************************
 -- customers table
@@ -10,4 +12,28 @@ CREATE TABLE customers
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   phone_number INTEGER NOT NULL
+);
+
+-- ************************************************************
+-- quotes table
+-- ************************************************************
+CREATE TABLE quotes
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  depature_location VARCHAR(255) NOT NULL,
+  destination_location VARCHAR(255) NOT NULL,
+  departure_date DATE NOT NULL,
+  return_date DATE NOT NULL,
+  status VARCHAR(255) NOT NULL
+);
+
+-- ************************************************************
+-- customers_quote table
+-- ************************************************************
+CREATE TABLE customers_quote
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  customer_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
+  bookier_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
+  quote_id INTEGER REFERENCES quotes(id) ON DELETE CASCADE
 );
