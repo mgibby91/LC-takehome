dd650bb54845a030e25c07df83afdb657aea95af
Matthew Gibson <mattgibson6@gmail.com>
2020-10-18 19:15:05 -0600

    clean up date and price for quote list items

--- LC-wetbat-client/src/components/quotes/QuotesListItem.jsx
+++ LC-wetbat-client/src/components/quotes/QuotesListItem.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 
 export default function QuotesListItem({
-  id,
   name,
   destination,
   price,
   departureDate
 }) {
 
+  let dollars = price / 100;
+  dollars = dollars.toLocaleString("en-US", { style: "currency", currency: "USD" });
+
+  const date = new Date(departureDate).toString().split(' ');
+  const finalDate = `${date[1]} ${date[2]}, ${date[3]}`
+
   return (
     <div className='quotes-list-item'>
       <div className="quotes-list-item-name">{name}</div>
       <div className="quotes-list-item-destination">{destination}</div>
-      <div className="quotes-list-item-price">{price}</div>
-      <div className="quotes-list-item-departure-date">{departureDate}</div>
+      <div className="quotes-list-item-price">{dollars}</div>
+      <div className="quotes-list-item-departure-date">{finalDate}</div>
     </div>
   )
 
