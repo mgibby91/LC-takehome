15d27a5a9fa9d20e449af88cb16b2d7998faa0e4
Matthew Gibson <mattgibson6@gmail.com>
2020-10-21 17:22:25 -0600

    add selected css functionality to side-nav

--- LC-wetbat-client/src/components/nav/SideNav.jsx
+++ LC-wetbat-client/src/components/nav/SideNav.jsx
@@ -4,16 +4,29 @@ import '../../styles/nav.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faDollarSign } from '@fortawesome/free-solid-svg-icons';
 
-export default function SideNav(props) {
+export default function SideNav({
+  selected,
+  setSelected
+}) {
+
+  // console.log('sidenav props', props);
 
   return (
     <div className="side-nav-container">
       <ul className="nav-links">
-        <Link to='/' className='side-nav-list-item' onClick={(e) => props.setSelected(e.target.textContent)}>
+        <Link
+          to='/'
+          className={selected === 'Home' ? 'side-nav-list-item side-nav-selected' : 'side-nav-list-item'}
+          onClick={() => setSelected('Home')}
+        >
           <FontAwesomeIcon icon={faHome} class='side-nav-icon' />
           <li>Home</li>
         </Link>
-        <Link to='/quotes' className='side-nav-list-item' onClick={(e) => props.setSelected(e.target.textContent)}>
+        <Link
+          to='/quotes'
+          className={selected === 'Quotes' ? 'side-nav-list-item side-nav-selected' : 'side-nav-list-item'}
+          onClick={() => setSelected('Quotes')}
+        >
           <FontAwesomeIcon icon={faDollarSign} class='side-nav-icon' />
           <li>Quotes</li>
         </Link>
