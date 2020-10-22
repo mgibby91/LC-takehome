31c63d2ff0a7d678d6870f01b8e2e6d8bdbc229e
Matthew Gibson <mattgibson6@gmail.com>
2020-10-22 10:46:56 -0600

    add in development icon to quick quote on home page

--- LC-wetbat-client/src/components/home/HomeQuickQuote.jsx
+++ LC-wetbat-client/src/components/home/HomeQuickQuote.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faForward } from '@fortawesome/free-solid-svg-icons';
+import { faForward, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 
 export default function HomeQuickQuote() {
 
@@ -13,7 +13,10 @@ export default function HomeQuickQuote() {
       </div>
 
       <div className="quick-quote-body">
-        <div className="quick-quote-body-text">In development</div>
+        <div className="quick-quote-body-text">
+          <FontAwesomeIcon icon={faExclamationTriangle} id='quotes-in-development-icon' />
+          In development
+          </div>
       </div>
     </div>
   )
--- LC-wetbat-client/src/components/quotes/QuotesView.jsx
+++ LC-wetbat-client/src/components/quotes/QuotesView.jsx
@@ -57,6 +57,7 @@ export default function QuotesView({
   }
 
   function clickDelete() {
+
     setState({ ...state, confirmDelete: true });
   }
 
