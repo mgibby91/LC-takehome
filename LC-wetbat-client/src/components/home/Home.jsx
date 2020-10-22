f57dfbda44410e05072632e2d5ff2f6385d7c5e8
Matthew Gibson <mattgibson6@gmail.com>
2020-10-22 10:23:44 -0600

    add initial layout for quick quote on home page

--- LC-wetbat-client/src/components/home/Home.jsx
+++ LC-wetbat-client/src/components/home/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import '../../styles/home.css';
+import HomeQuickQuote from './HomeQuickQuote';
 import axios from 'axios';
 import { useEffect } from 'react';
+
 
 export default function Home(props) {
 
@@ -25,15 +27,19 @@ export default function Home(props) {
 
   return (
     <div className='home-main-container'>
+
       <div className="extended-card-container">
         <div className="main-card-left">
           <div className="main-card-left-title">Welcome to your dashboard</div>
           <div className="main-card-left-text">What's the status on the deliverables for eow? we need a paradigm shift. Ping me meeting assassin run it up the flagpole, ping the boss and circle back. Programmatically message the initiative let's not solutionize this right now parking lot it. Where the metal hits the meat shotgun approach we need to harvest synergy effects. We need to crystallize a plan to be inspired is to become creative, innovative and energized we want this philosophy to trickle down to all our stakeholders nor UI, yet get six alpha pups in here for a focus group, but driving the initiative forward.</div>
         </div>
-        <div className="main-card-right">
-
-        </div>
+        <div className="main-card-right"></div>
       </div>
+
+      <HomeQuickQuote
+
+      />
+
     </div>
   )
 
---
+++ LC-wetbat-client/src/components/home/HomeQuickQuote.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faForward } from '@fortawesome/free-solid-svg-icons';
+
+export default function HomeQuickQuote() {
+
+  return (
+    <div className="home-card-container">
+
+      <div className="quotes-list-title">
+        <FontAwesomeIcon icon={faForward} id='quotes-main-icon' />
+        <div className="quotes-list-title-title">Quick Quote</div>
+      </div>
+
+      <div className="quick-quote-body">
+
+      </div>
+    </div>
+  )
+
+}
