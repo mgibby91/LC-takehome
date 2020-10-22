042cc49ea1096807033137d77f3251b788f9333b
Matthew Gibson <mattgibson6@gmail.com>
2020-10-22 20:48:34 +0000

    npm run build command ran

---
+++ LC-wetbat-client/build/precache-manifest.8f11390d68f4d79f06c3aec2378905aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda0fe4b77a4e21ca4f2a3ff0bf05b1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77c9812e5c82b2f3b353",
+    "url": "/static/css/main.7ecd3f64.chunk.css"
+  },
+  {
+    "revision": "93bd13ae64b6465f4a4f",
+    "url": "/static/js/2.351365be.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/static/js/2.351365be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77c9812e5c82b2f3b353",
+    "url": "/static/js/main.6c5dd150.chunk.js"
+  },
+  {
+    "revision": "e2c6e531b8324ac17d6c",
+    "url": "/static/js/runtime-main.206ac5b9.js"
+  }
+]);
