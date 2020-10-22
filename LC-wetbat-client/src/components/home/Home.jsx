9ad3d028d3f0b2abcadc28eb846d53a3c35356ea
Matthew Gibson <mattgibson6@gmail.com>
2020-10-22 09:38:12 -0600

    add main-card on home page with initial styling/layout

--- LC-wetbat-client/src/components/home/Home.jsx
+++ LC-wetbat-client/src/components/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../../styles/home.css';
 import axios from 'axios';
 import { useEffect } from 'react';
 
@@ -24,7 +25,15 @@ export default function Home(props) {
 
   return (
     <div className='home-main-container'>
-      Home!
+      <div className="extended-card-container">
+        <div className="main-card-left">
+          <div className="main-card-left-title">Welcome to your dashboard</div>
+          <div className="main-card-left-text">What's the status on the deliverables for eow? we need a paradigm shift. Ping me meeting assassin run it up the flagpole, ping the boss and circle back. Programmatically message the initiative let's not solutionize this right now parking lot it. Where the metal hits the meat shotgun approach we need to harvest synergy effects. We need to crystallize a plan to be inspired is to become creative, innovative and energized we want this philosophy to trickle down to all our stakeholders nor UI, yet get six alpha pups in here for a focus group, but driving the initiative forward.</div>
+        </div>
+        <div className="main-card-right">
+
+        </div>
+      </div>
     </div>
   )
 
