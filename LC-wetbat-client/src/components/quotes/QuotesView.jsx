be2af88de0ceb20f0d036b4075c59035a9b3afdf
Matthew Gibson <mattgibson6@gmail.com>
2020-10-21 19:48:10 -0600

    initial functionality for dispalying quote details

--- LC-wetbat-client/src/components/quotes/QuotesView.jsx
+++ LC-wetbat-client/src/components/quotes/QuotesView.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../../styles/quotes.css';
+import dollarsConverter from '../../helpers/dollarsConverter';
+import humanReadableDate from '../../helpers/humanReadableDate';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faForward } from '@fortawesome/free-solid-svg-icons';
 
@@ -46,9 +48,15 @@ export default function QuotesView({
       }
     }
 
-
-
   }, [currentQuoteID]);
+
+  function setTransportationList(transportationArr) {
+    let finalStr = '';
+    for (let trans of transportationArr) {
+      finalStr += `${trans}, `;
+    }
+    return finalStr.slice(0, -2);
+  }
 
   return (
     <div className="card-container">
@@ -69,33 +77,33 @@ export default function QuotesView({
         </div>
         <div className="quotes-view-item">
           <div className="quotes-view-item-title">DEPARTURE DATE</div>
-          <div className="quotes-view-item-content"></div>
+          <div className="quotes-view-item-content">{state.departureDate ? humanReadableDate(state.departureDate) : ''}</div>
         </div>
         <div className="quotes-view-item">
           <div className="quotes-view-item-title">RETURN DATE</div>
-          <div className="quotes-view-item-content"></div>
+          <div className="quotes-view-item-content">{state.departureDate ? humanReadableDate(state.returnDate) : ''}</div>
         </div>
         <div className="quotes-view-item">
-          <div className="quotes-view-item-title">TRAVELLERS NO.</div>
-          <div className="quotes-view-item-content"></div>
+          <div className="quotes-view-item-title">TRAVELLERS NUMBER</div>
+          <div className="quotes-view-item-content">{state.travellersNumber}</div>
         </div>
         <div className="quotes-view-item">
           <div className="quotes-view-item-title">TRANSPORTATION</div>
-          <div className="quotes-view-item-content"></div>
+          <div className="quotes-view-item-content">{setTransportationList(state.transportation)}</div>
         </div>
         <div className="quotes-view-item">
           <div className="quotes-view-item-title">NAME</div>
-          <div className="quotes-view-item-content"></div>
+          <div className="quotes-view-item-content">{state.name}</div>
         </div>
         <div className="quotes-view-item">
           <div className="quotes-view-item-title">PRICE</div>
-          <div className="quotes-view-item-content"></div>
+          <div className="quotes-view-item-content">{state.priceCents ? dollarsConverter(state.priceCents) : ''}</div>
         </div>
         <div className="quotes-view-item">
           <div className="quotes-view-item-title">CONTACT INFO</div>
-          <div className="quotes-view-item-content"></div>
+          <div className="quotes-view-item-content">{state.email ? state.email + ' / ' + state.phoneNumber : ''}</div>
         </div>
-        <div className="quotes-view-item">
+        <div className="quotes-view-item" id='quotes-view-btns'>
 
         </div>
       </div>
